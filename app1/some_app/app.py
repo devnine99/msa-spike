c5d02cec8ff76d74fcdb9863d5b84eaff5c9b2a7
devnine99 <devnine99@gmail.com>
2021-11-04 16:05:40 +0900

    initial commit

--- app1/some_app/app.py
+++ app1/some_app/app.py
@@ -6,8 +6,8 @@ from some_app.event import UnKnownEvent
 class SomeApp:
     def __init__(self, topic):
         self.consumer = KafkaConsumer(topic)
-        self.event_classes = []
-        self.event_classes += self.autodiscover_event()
+        self.event_classes = {}
+        # self.event_classes += self.autodiscover_event()
 
     def run(self):
         for message in self.consumer:
@@ -16,16 +16,15 @@ class SomeApp:
     def config(self, **kwargs):
         self.consumer.config.update(**kwargs)
 
-    @staticmethod
-    def discover_event(*classes):
-        return classes
+    def discover_event(self, *classes):
+        self.event_classes.update({cls.key: cls for cls in classes})
 
-    def autodiscover_event(self):
-        # 장고앱에서 Event 클래스를 상속받은 클래스 모두 가져오도록
-        return []
+    # def autodiscover_event(self):
+    #     # 장고앱에서 Event 클래스를 상속받은 클래스 모두 가져오도록
+    #     return []
 
     def get_event(self, message):
-        for event_class in self.event_classes:
-            if event_class.check_key(message.key):
-                return event_class(message)
-        return UnKnownEvent(message)
+        try:
+            return self.event_classes[message.key](message)
+        except KeyError:
+            return UnKnownEvent(message)
