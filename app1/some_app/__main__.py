5281ef558ac247edf0ca5270eecf5c0fa581fc6d
devnine99 <devnine99@gmail.com>
2021-11-05 11:37:28 +0900

    패키지내에서 실행해주도록 설계 변경

--- app1/some_app/__main__.py
+++ app1/some_app/__main__.py
@@ -3,12 +3,13 @@ import importlib.util
 
 from some_app.app import SomeApp
 
-BIN_NAME = 'some_app.py'  # TODO: bin에 등록되면 고정(someapp)
+BIN_NAME = 'some_app.py'  # TODO: bin에 등록되면 고정(some_app)
 
 
 def main():
+    app = 'config.events'  # TODO: command 입력 받도록 ex) some_app -A config.events
     module_name = 'some_app'
-    module_path = sys.argv[0].replace(BIN_NAME, 'config/events.py')  # TODO: command 입력 받도록 ex) someapp -A config.events
+    module_path = sys.argv[0].replace(BIN_NAME, f'{app.replace(".", "/")}.py')
 
     spec = importlib.util.spec_from_file_location(module_name, module_path)
     mod = importlib.util.module_from_spec(spec)
